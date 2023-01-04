fa615dc8b94fc8a6626af8f8fdf6146f694f59a1
Rajan <rajan22091998@gmail.com>
2023-01-04 10:41:43 +0530

    added correlation.R script

---
+++ basic_plots/correlation.R
@@ -0,0 +1,15 @@
+# Importing modules
+library("corrplot")
+
+# Reading data
+data <- read.table(file="dummy_data1.csv", header = TRUE, sep = ",")
+
+# Defining image parameter
+png(file = "correlation.png", width=500, height=400)
+
+# Plotting correlation plot
+corrplot(cor(data[-1]))
+
+# Saving image
+dev.off()
+
