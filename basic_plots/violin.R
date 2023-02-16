8e3cb61944d3510e01897433f578371d0886764d
Rajan <rajan22091998@gmail.com>
2023-02-16 09:56:28 +0530

    added violin.R script

---
+++ basic_plots/violin.R
@@ -0,0 +1,23 @@
+# Importing package
+library(ggplot2)
+library(reshape2)
+
+# Reading datasets
+data <- read.csv("dummy_data1.csv", sep=",", header=T)
+
+# Matrix manipulation
+mat <- as.matrix(data[, -1])
+rownames(mat) <- data$Sample
+
+# Defining image parms
+png(file = "violin.png", width=600, height=500)
+
+# Plotting violin plot
+ggplot(melt(mat), aes(x=Var2, y=value, group=Var2, color=Var2)) + 
+	geom_violin(size=1) + geom_boxplot(width=0.1)+
+	theme(panel.background=element_blank(),panel.border = element_rect(colour = "black", fill=NA, size=1 ), 
+	axis.text.x = element_text(size = 12, angle = 90),axis.text.y = element_text(size = 12, angle = 90), legend.position = "none")+
+	ggtitle(" ")+xlab("Genes")+ylab("Quantity")
+	
+# Saving image
+dev.off()
